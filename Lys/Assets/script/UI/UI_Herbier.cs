b5bf818888e23ae596dceba1f44ce5a95ca00369
ClementProject <cousseauclement@gmail.com>
2020-03-27 14:40:43 +0100

    UI_herbier

---
+++ Lys/Assets/script/UI/UI_Herbier.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI_Herbier : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject carte;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OpenHerbier()
+    {
+        carte.gameObject.SetActive(true);
+    }
+}
